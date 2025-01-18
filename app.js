71ae579fffc813fe8d7c32a443de5347f6159d47
Osteenjr <osteenonyeke@gmail.com>
2025-01-18 21:37:31 +0100

    Updated Files

---
+++ app.js
@@ -0,0 +1,12 @@
+// ====SIDEBAR=====
+const menuBtn = document.querySelector('.menubtn');
+const closeBtn = document.querySelector('.closebtn');
+const sideBar = document.querySelector('.sidebar');
+
+function showMenu() {
+    sideBar.style.display = 'flex';
+}
+
+function hideMenu() {
+    sideBar.style.display = 'none';
+}
